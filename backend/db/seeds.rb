5d4e105a234f73720c2c8fa2ed9c8d8f1edec1c9
Felipe <rocha.felipe98@gmail.com>
2025-07-11 19:11:19 -0300

    feat(db): add vehicle leasing options table

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,13 +1,121 @@
-# db/seeds.rb
-Car.create!(
-  brand: "Toyota",
-  model: "Corolla",
-  year: 2020,
-  color: "Prata",
-  mileage: 32000,
-  price: 85000.0,
-  images: [
-    "https://omecanico.com.br/wp-content/uploads/2019/09/Toyota-Corolla-2020_1.jpg",
-    "https://quatrorodas.abril.com.br/wp-content/uploads/2019/03/painel-3-toyota-corolla.jpg?quality=70\\u0026strip=info\\u0026w=720\\u0026crop=1"
-  ].to_json
-)
+Car.create!([
+  {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    color: "Prata",
+    mileage: 32000,
+    price: 85000.0,
+    images: [
+      "https://omecanico.com.br/wp-content/uploads/2019/09/Toyota-Corolla-2020_1.jpg",
+      "https://quatrorodas.abril.com.br/wp-content/uploads/2019/03/painel-3-toyota-corolla.jpg?quality=70&strip=info&w=720&crop=1"
+    ].to_json
+  },
+  {
+    brand: "Honda",
+    model: "Civic",
+    year: 2019,
+    color: "Preto",
+    mileage: 45000,
+    price: 78000.0,
+    images: [
+      "https://www.webmotors.com.br/imagens/prod/346781/HONDA_CIVIC_2.0_16V_FLEXONE_SPORT_4P_CVT_34678116212998096.jpg",
+      "https://www.webmotors.com.br/imagens/prod/346781/HONDA_CIVIC_2.0_16V_FLEXONE_SPORT_4P_CVT_34678116213041327.jpg?s=fill&w=440&h=330&q=80&t=true"
+    ].to_json
+  },
+  {
+    brand: "Chevrolet",
+    model: "Onix",
+    year: 2021,
+    color: "Branco",
+    mileage: 25000,
+    price: 70000.0,
+    images: [
+      "https://s3.ecompletocarros.dev/images/lojas/232/veiculos/176074/veiculoInfoVeiculoImagesMobile/vehicle_image_1705668368_03586d39fb5459f08f3eea27e394e0e5.jpeg",
+      "https://image1.mobiauto.com.br/images/api/images/v1.0/41864704/transform/fl_progressive,f_webp,q_85,w_959"
+    ].to_json
+  },
+  {
+    brand: "Ford",
+    model: "Ka",
+    year: 2018,
+    color: "Cinza",
+    mileage: 60000,
+    price: 48000.0,
+    images: [
+      "https://image1.mobiauto.com.br/images/api/images/v1.0/79638338/transform/fl_progressive,f_webp,q_85,w_959",
+      "https://i0.statig.com.br/bancodeimagens/imgalta/b4/hl/wz/b4hlwz2ihzwuv3fehns2fy1c8.jpg"
+    ].to_json
+  },
+  {
+    brand: "Volkswagen",
+    model: "Gol",
+    year: 2022,
+    color: "Vermelho",
+    mileage: 15000,
+    price: 62000.0,
+    images: [
+      "https://garagem360.com.br/wp-content/uploads/2021/05/7d42d57c-3ded-4e0a-90d2-1169d45bf397.jpg",
+      "https://fotos-jornaldocarro-estadao.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/09/10183007/VW-Gol.jpg"
+    ].to_json
+  },
+  {
+    brand: "Hyundai",
+    model: "HB20",
+    year: 2020,
+    color: "Azul",
+    mileage: 28000,
+    price: 69000.0,
+    images: [
+      "https://cdn.motor1.com/images/mgl/qN86g/s1/4x3/hyundai-hb20s-2020.webp",
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202407/20240730/hyundai-hb20-1.0-12v-flex-sense-plus-manual-wmimagem17594645078.jpg"
+    ].to_json
+  },
+  {
+    brand: "Fiat",
+    model: "Argo",
+    year: 2021,
+    color: "Branco",
+    mileage: 21000,
+    price: 63000.0,
+    images: [
+      "https://image1.mobiauto.com.br/images/api/images/v1.0/46735166/transform/fl_progressive,f_webp,q_auto",
+      "https://carango.com.br/f.php?w=810&h=620&cc=0&zc=1&src=https://carango.nyc3.digitaloceanspaces.com/images/veiculos/2022/06/fiat-argo-2021-1-0-firefly-flex-drive-manual-flex-103925-xlmnkt.jpg"
+    ].to_json
+  },
+  {
+    brand: "Renault",
+    model: "Kwid",
+    year: 2022,
+    color: "Laranja",
+    mileage: 10000,
+    price: 56000.0,
+    images: ["https://image1.mobiauto.com.br/images/api/images/v1.0/49021141/transform/fl_progressive,f_webp,q_85,w_959","https://s3.ecompletocarros.dev/images/lojas/217/veiculos/166379/veiculoInfoVeiculoImagesMobile/vehicle_image_1700254809_d41d8cd98f00b204e9800998ecf8427e.jpeg"
+
+    ].to_json
+  },
+  {
+    brand: "Nissan",
+    model: "Versa",
+    year: 2021,
+    color: "Prata",
+    mileage: 30000,
+    price: 77000.0,
+    images: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxpfoAltaDQXcMJrgjpOSFDrcAcwVA_xKq7w&s",
+      "https://revistacarro.com.br/wp-content/uploads/2020/10/versa-2021-4.jpg"
+    ].to_json
+  },
+  {
+    brand: "Jeep",
+    model: "Renegade",
+    year: 2020,
+    color: "Verde Militar",
+    mileage: 35000,
+    price: 99000.0,
+    images: [
+      "https://s2-autoesporte.glbimg.com/iBAp5h7GogSVydqC_i0qhpg2IS4=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/4/F/XlWQLsQnyg8ycGKmSRHw/2019-03-18-imagem.jpg",
+      "https://dezeroacem.com.br/wp-content/uploads/2019/09/Jeep-Renegade-Sport-2020-Night-Eagle-painel.jpg"
+    ].to_json
+  }
+])
