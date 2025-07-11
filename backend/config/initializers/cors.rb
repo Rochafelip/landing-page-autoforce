200e02b58880b6e8d6f184f6cb2815a072d3fc4b
Felipe <rocha.felipe98@gmail.com>
2025-07-11 19:10:42 -0300

    config(cors): allow staging domain temporarily

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins "http://localhost:5173"
+     origins "http://127.0.0.1:5173"
 
     resource "*",
-       headers: :any,
-       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization']
    end
 end
